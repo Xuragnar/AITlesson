9cf5e315154a84aa6aead97710d17d43a13a9750
Sergey <makeev.2006@gmail.com>
2023-08-08 18:19:40 +0200

    HWork14

---
+++ src/homework14/HWork14.java
@@ -0,0 +1,12 @@
+package homework14;
+
+public class HWork14 {
+    public static void main(String[] args) {
+
+        ModelClass car = new ModelClass(15, 2, "Skoda");
+        System.out.println("Car age " + car.age);
+        System.out.println("Car stage " + car.stages);
+        System.out.println("Car model " + car.model);
+    }
+
+}
---
+++ src/homework14/ModelClass.java
@@ -0,0 +1,17 @@
+package homework14;
+
+public class ModelClass {
+
+
+    String model;
+    int stages;
+    int age;
+public ModelClass (int age, int stages, String model){
+
+    this.model=model;
+    this.stages=stages;
+    this.age=age;
+
+
+}
+}
